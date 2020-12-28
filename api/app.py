21a8b62486e560b3417ade7d584bc87451e9424a
Yk(open) <ykmoon0814@gmail.com>
2020-12-29 01:11:37 +0900

    28.Dec

--- api/app.py
+++ api/app.py
@@ -1,9 +1,19 @@
 from flask import Flask,jsonify,request
+from flask.json import JSONEncoder
+
 app=Flask(__name__)
 app.id_count=1
 app.users={}
 app.tweets=[]
 
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self,obj):
+        if isinstance(obj,set):
+            return list(obj)
+        return JSONEncoder.default(self,obj)
+
+app.json_encoder=CustomJSONEncoder
 @app.route("/ping",methods=['GET'])
 def ping():
     return "pong"
@@ -27,3 +37,16 @@ def tweet():
         return "300자 초과",400
     app.tweets.append({'user_id':user_id,'tweet':tweet})
     return '',200
+
+@app.route('/follow',methods=['POST'])
+def follow():
+    payload=request.json
+    user_id=int(payload['id'])
+    user_id_to_follow=int(payload['follow'])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return "유저가 없습니다.",400
+    user=app.users[user_id]
+    user.setdefault('follow',set()).add(user_id_to_follow)
+
+    return jsonify(user)
