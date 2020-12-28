1ad8da77715ffd8049a7225e9e832b4da9fac1c9
Yk(open) <ykmoon0814@gmail.com>
2020-12-29 00:56:48 +0900

    29.Dec

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask,jsonify,request
 app=Flask(__name__)
 app.id_count=1
 app.users={}
+app.tweets=[]
 
 @app.route("/ping",methods=['GET'])
 def ping():
@@ -15,3 +16,14 @@ def sign_up():
     app.id_count=app.id_count+1
 
     return jsonify(new_user)
+
+@app.route("/tweet",methods=['POST'])
+def tweet():
+    upload_info=request.json
+    user_id=int(upload_info['id'])
+    tweet=upload_info["tweet"]
+
+    if len(tweet)>300:
+        return "300자 초과",400
+    app.tweets.append({'user_id':user_id,'tweet':tweet})
+    return '',200
